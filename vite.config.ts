79dcd1662b38ec6f40266db3177e69a7be2e748d
Bartosz Zakrzewski <bartoszzakrzewski@mti.edu.pl>
2026-04-11 00:53:27 +0200

    GITHUB IS STINKY POO

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineconfig } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componenttagger } from "lovable-tagger";
+import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineconfig(({ mode }) => ({
+export default defineConfig(({ mode }) => ({
   base: "/blossom-eco-scents",
   server: {
     host: "::",
@@ -13,7 +13,7 @@ export default defineconfig(({ mode }) => ({
       overlay: false,
     },
   },
-  plugins: [react(), mode === "development" && componenttagger()].filter(boolean),
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
