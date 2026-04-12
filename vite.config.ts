75e99e4ecb536bdd660063cc1411380b9e882dad
unknown <eadrian321plock@gmail.com>
2026-04-12 14:50:17 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/blossom-eco-scents",
+  base: "/blossom-eco-scents/",
   server: {
     host: "::",
     port: 8080,
